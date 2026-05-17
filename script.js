73b42ccdb34611f6e6e167ec77a29ff6d00651cd
Yulia Novskaya <amazona27@mail.ru>
2026-05-17 15:07:13 +0800

    MVP 1 | add sticky header scroll effect

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+const header = document.querySelector(".site-header");
 
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 30) {
+    header.classList.add("header-scrolled");
+  } else {
+    header.classList.remove("header-scrolled");
+  }
+});
